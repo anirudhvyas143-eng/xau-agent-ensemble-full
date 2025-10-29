456dc7826949746dbe99a98b7d090be0ecdef2a3
anirudhvyas143-eng <anirudhvyas143@gmail.com>
2025-10-30 01:58:05 +0530

    Delete setup_folders.py

--- setup_folders.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for folder in ["data", "models", "logs", "history"]:
-    os.makedirs(folder, exist_ok=True)
-    open(os.path.join(folder, ".gitkeep"), "a").close()
-
-print("✅ All folders created with .gitkeep files!")
